19b9dcae954ac151e4db7cbb8f7d397b6afd01a3
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-04-04 13:45:39 +0530

    added happy or sad

---
+++ src/main/java/MoodAnalyserMain.java
@@ -0,0 +1,22 @@
+/**
+ * Purpose  -  ability to analyse and respond Happy or Sad Mood
+ * @author  - Ashvini Kanojia
+ * @version - 16.0
+ * @since   - 01-04-2022
+ */
+
+/**
+ * create a class name as MoodAnalyserMain
+ */
+public class MoodAnalyserMain {
+    /**
+     * create a constructor ,this is default constructor name as MoodAnalyserMain
+     */
+
+    public String analyseMood(String message) {
+        if (message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
+}
